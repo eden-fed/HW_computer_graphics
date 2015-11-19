9a800d22b76c6ada8447cc35ab9db70fa721277e
Eden <eden.fadid@gmail.com>
2015-11-19 17:17:43 +0200

    meshmodel

--- CG/MeshModel.cpp
+++ CG/MeshModel.cpp
@@ -4,8 +4,21 @@ MeshModel::MeshModel()
 {
 }
 
-MeshModel::MeshModel(Wavefront_obj &)
+MeshModel::MeshModel(Wavefront_obj & J)
 {
+	vertexInfo v;
+	bool has_normals;
+	if (J.m_normals.size() > 0) {
+		has_normals = true;
+	}
+	for (int i = 0; i < J.m_points.size(); i++) {
+		v.vertex = J.m_points[i];
+		if (has_normals)
+			v.normal = J.m_normals[i];
+		else
+			CalcNormals();
+		vertices.push_back(v);
+	}
 }
 
 MeshModel::~MeshModel()
