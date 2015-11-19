1b0ba5fbe3e6dfdafd3090c89472ce0ca9fc4509
ben <ben123678@gmail.com>
2015-11-19 17:20:37 +0200

    added calcCentroid to meshmodel

--- CG/MeshModel.cpp
+++ CG/MeshModel.cpp
@@ -16,3 +16,12 @@ Vector4 MeshModel::getCentroid()
 {
 	return this->centroid;
 }
+
+Vector4 MeshModel::calcCentroid()
+{
+	Vector4 sum;
+	for (int i = 0; i < vertices.size();i++) {
+		sum += vertices[i].vertex;
+	}
+	return sum *(1 / vertices.size());
+}
