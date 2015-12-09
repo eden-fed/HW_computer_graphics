1824a00e694cfd745ddfb5aca1fa901e3d88f2f6
Bmortkov <ben.mortkovitch@intel.com>
2015-12-09 11:02:57 +0200

    added class material

---
+++ CG/Material.cpp
@@ -0,0 +1,31 @@
+#include "Material.h"
+
+Material::Material(double A, double D, double S)
+{
+	this->ambient = A;
+	this->diffuse = D;
+	this->specular = S;
+}
+
+Material::Material()
+{
+}
+
+Material::~Material()
+{
+}
+
+void Material::setAmbient(double A)
+{
+	this->ambient = A;
+}
+
+void Material::setDiffuse(double D)
+{
+	this->diffuse = D;
+}
+
+void Material::setSpecular(double S)
+{
+	this->specular = S;
+}
