f705035f6bc047dcdc210597035dce8331493a13
Bmortkov <ben.mortkovitch@intel.com>
2015-12-14 11:01:53 +0200

    added z buffer depth method

--- CG/Z_Buffer.cpp
+++ CG/Z_Buffer.cpp
@@ -15,5 +15,15 @@ Z_Buffer::~Z_Buffer()
 
 double Z_Buffer::Depth(Triangle Q, double X, double Y)
 {
-	return 0.0;
+	//Ax + By +Cz + D = 0 Normal Vector: N=(A, B, C)
+	Vector4 N = Q.getNormal();
+
+	double A = N[0];
+	double B = N[1];
+	double C = N[2];
+	double D = -(A*Q[1][0] + B*Q[1][0] + C*Q[1][0]);
+
+
+	//Z = (-AX -BY -D)/C
+	return -((A*X) + (B*Y) + D) / C;
 }
