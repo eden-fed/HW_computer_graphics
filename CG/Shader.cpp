a88f5bb5159571ec429af20e06ade746947f6f7d
Ben <ben123678@gmail.com>
2015-12-26 20:43:43 +0200

    fixed specular camera position

--- CG/Shader.cpp
+++ CG/Shader.cpp
@@ -27,28 +27,28 @@ Shader::~Shader()
 {
 }
 
-void Shader::draw(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer)
+void Shader::draw(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer, Vector4& eyePosition)
 {
 	switch (ShadingType) {
 	case FLAT:
-		flatShading(mesh, ambientLight, light1, light2, zBuffer);
+		flatShading(mesh, ambientLight, light1, light2, zBuffer, eyePosition);
 		break;
 	case GOURAUD:
-		gouraudShading(mesh, ambientLight, light1, light2, zBuffer);
+		gouraudShading(mesh, ambientLight, light1, light2, zBuffer, eyePosition);
 		break;
 	case PHONG:
-		phongShading(mesh, ambientLight, light1, light2, zBuffer);
+		phongShading(mesh, ambientLight, light1, light2, zBuffer, eyePosition);
 		break;
 	}
 }
 
-void Shader::flatShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer)
+void Shader::flatShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer, Vector4& eyePosition)
 {
 		for (int i = 0; i < mesh.getAllFaces().size(); i++) {
 			//clr.setColor(clr.getColor() + 0x05050500);
 			Triangle& T = mesh.getAllFaces()[i];
 
-			Color clr = getFlatColor(T, mesh.material, ambientLight, light1, light2);
+			Color clr = getFlatColor(T, mesh.material, ambientLight, light1, light2, eyePosition);
 
 			//bounding rectangle parameters
 			double minX, maxX, minY, maxY;
@@ -88,15 +88,15 @@ void Shader::flatShading(MeshModel& mesh, Color ambientLight, Light& light1, Lig
 		}
 }
 
-void Shader::gouraudShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer & zBuffer)
+void Shader::gouraudShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer & zBuffer, Vector4& eyePosition)
 {
 	for (int i = 0; i < mesh.getAllFaces().size(); i++) {
 		Triangle& T = mesh.getAllFaces()[i];
 
 		//calculate Trinagle vertex colors
-		Color clr0 = getVertxColor(T.getVertexInfo(0), mesh.material, ambientLight, light1, light2);
-		Color clr1 = getVertxColor(T.getVertexInfo(1), mesh.material, ambientLight, light1, light2);
-		Color clr2 = getVertxColor(T.getVertexInfo(2), mesh.material, ambientLight, light1, light2);
+		Color clr0 = getVertxColor(T.getVertexInfo(0), mesh.material, ambientLight, light1, light2, eyePosition);
+		Color clr1 = getVertxColor(T.getVertexInfo(1), mesh.material, ambientLight, light1, light2, eyePosition);
+		Color clr2 = getVertxColor(T.getVertexInfo(2), mesh.material, ambientLight, light1, light2, eyePosition);
 
 
 		//bounding rectangle parameters
@@ -143,7 +143,7 @@ void Shader::gouraudShading(MeshModel& mesh, Color ambientLight, Light& light1, 
 	}
 }
 
-void Shader::phongShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer & zBuffer)
+void Shader::phongShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer & zBuffer, Vector4& eyePosition)
 {
 	//loop through all the triangles
 	for (int i = 0; i < mesh.getAllFaces().size(); i++) {
@@ -187,7 +187,7 @@ void Shader::phongShading(MeshModel& mesh, Color ambientLight, Light& light1, Li
 					vInfo.normal = vInfo.normal.normalize();
 
 					//claculate the color
-					Color clr = getVertxColor(vInfo, mesh.material, ambientLight, light1, light2);
+					Color clr = getVertxColor(vInfo, mesh.material, ambientLight, light1, light2, eyePosition);
 
 					crdInfo.clr.setColor(clr.getColor());
 					crdInfo.trl = T;
@@ -247,25 +247,17 @@ double Shader::helpGNBC(Triangle& T, int a, int b, double x, double y)
 	return ((T[a][Y] - T[b][Y])*x) + ((T[b][X] - T[a][X])*y) + T[a][X] * T[b][Y] - T[b][X] * T[a][Y];
 }
 
-Color Shader::getFlatColor(Triangle & T, Material& M, Color& ambientLight, Light & light1, Light & light2)
+Color Shader::getFlatColor(Triangle & T, Material& M, Color& ambientLight, Light & light1, Light & light2, Vector4& eyePosition)
 {
-	
+	vertexInfo vInfo;
+
 	double A = M.getAmbient();
-	Vector4 tCentroid = ((T[0] + T[1]) + T[2]) / 3.0;
-
-	Color ambient(ambientLight.getRedPortion()*A, ambientLight.getGreenPortion()*A, ambientLight.getBluePortion()*A);
-
-	Color diffuse1 = clacDiffuseLight(tCentroid, T.getNormal(), light1, M.getDiffuse());
-	Color specular1 = clacSpecularLight(tCentroid, T.getNormal(), light1, M.getSpecular(), M.getspecularExp());
-	Color diffuse2 = clacDiffuseLight(tCentroid, T.getNormal(), light2, M.getDiffuse());
-	Color specular2 = clacSpecularLight(tCentroid, T.getNormal(), light2, M.getSpecular(), M.getspecularExp());
-	Color color((ambient.getRedPortion() + diffuse1.getRedPortion() + diffuse2.getRedPortion() + specular1.getRedPortion() + specular2.getRedPortion()),
-				(ambient.getGreenPortion() + diffuse1.getGreenPortion() + diffuse2.getGreenPortion() + specular1.getGreenPortion() + specular2.getGreenPortion()),
-				(ambient.getBluePortion() + diffuse1.getBluePortion() + diffuse2.getBluePortion() + specular1.getBluePortion() + specular2.getBluePortion()));
-	return color;
+	vInfo.vertex = ((T[0] + T[1]) + T[2]) / 3.0; //tCentroid
+	vInfo.normal = T.getNormal();
+	return getVertxColor(vInfo, M, ambientLight, light1, light2, eyePosition);
 }
 
-Color Shader::getVertxColor(vertexInfo& vInfo, Material & M, Color & ambientLight, Light & light1, Light & light2)
+Color Shader::getVertxColor(vertexInfo& vInfo, Material & M, Color & ambientLight, Light & light1, Light & light2, Vector4& eyePosition)
 {
 	double A = M.getAmbient();
 	Vector4& P = vInfo.vertex;
@@ -274,9 +266,9 @@ Color Shader::getVertxColor(vertexInfo& vInfo, Material & M, Color & ambientLigh
 	Color ambient(ambientLight.getRedPortion()*A, ambientLight.getGreenPortion()*A, ambientLight.getBluePortion()*A);
 
 	Color diffuse1  = clacDiffuseLight(P, N, light1, M.getDiffuse());
-	Color specular1 = clacSpecularLight(P, N, light1, M.getSpecular(), M.getspecularExp());
+	Color specular1 = clacSpecularLight(P, N, light1, M.getSpecular(), M.getspecularExp(),eyePosition);
 	Color diffuse2  = clacDiffuseLight(P, N, light2, M.getDiffuse());
-	Color specular2 = clacSpecularLight(P, N, light2, M.getSpecular(), M.getspecularExp());
+	Color specular2 = clacSpecularLight(P, N, light2, M.getSpecular(), M.getspecularExp(), eyePosition);
 	Color color((ambient.getRedPortion() + diffuse1.getRedPortion() + diffuse2.getRedPortion() + specular1.getRedPortion() + specular2.getRedPortion()),
 		(ambient.getGreenPortion() + diffuse1.getGreenPortion() + diffuse2.getGreenPortion() + specular1.getGreenPortion() + specular2.getGreenPortion()),
 		(ambient.getBluePortion() + diffuse1.getBluePortion() + diffuse2.getBluePortion() + specular1.getBluePortion() + specular2.getBluePortion()));
@@ -286,7 +278,7 @@ Color Shader::getVertxColor(vertexInfo& vInfo, Material & M, Color & ambientLigh
 Color Shader::clacDiffuseLight(Vector4& point, Vector4 & normal, Light& light, double Kd)
 {
 
-	Vector4 N =normal.normalize();
+	Vector4 N =normal;
 	Vector4 L;
 
 	//get light direction
@@ -296,7 +288,6 @@ Color Shader::clacDiffuseLight(Vector4& point, Vector4 & normal, Light& light, d
 		L = light.getPosition() - point;
 	//normalize light
 	L = L.normalize();
-	N[3] = L[3] = 0;
 
 	//calc NL
 	double NL = (N*L);
@@ -308,9 +299,9 @@ Color Shader::clacDiffuseLight(Vector4& point, Vector4 & normal, Light& light, d
 	return retColor;
 }
 
-Color Shader::clacSpecularLight(Vector4& point, Vector4& normal, Light& light, double Ks, double specularExp)
+Color Shader::clacSpecularLight(Vector4& point, Vector4& normal, Light& light, double Ks, double specularExp, Vector4& eyePosition)
 {
-	Vector4 N = normal.normalize();
+	Vector4 N = normal;
 	Vector4 L;
 	//get light direction
 	if (light.getType() == _DIRECTION)
@@ -320,7 +311,6 @@ Color Shader::clacSpecularLight(Vector4& point, Vector4& normal, Light& light, d
 
 	//normalize light
 	L = L.normalize();
-	N[3] = 0; L[3] = 0;
 
 	double dotNL = (N*L);
 	dotNL = (dotNL) < 0 ? (0) : (dotNL);
@@ -329,8 +319,8 @@ Color Shader::clacSpecularLight(Vector4& point, Vector4& normal, Light& light, d
 	Vector4 R = (N * (dotNL*2)) - L;
 	R = R.normalize();// = R*(1 / R.getSize());
 
-	//calculate 
-	Vector4 V = point*(-1); //eye - point 
+	//calculate V
+	Vector4 V = eyePosition-point; 
 	V = V.normalize();
 
 	double RV = (R*V);
--- CG/Shader.h
+++ CG/Shader.h
@@ -31,21 +31,21 @@ public:
 	~Shader();
 
 	//class methods
-	void draw(MeshModel& mesh,Color ambientLight, Light& light1, Light& light2,Z_Buffer& zBuffer);
+	void draw(MeshModel& mesh,Color ambientLight, Light& light1, Light& light2,Z_Buffer& zBuffer,Vector4& eyePosition);
 
-	void flatShading(MeshModel& mesh, Color ambientLight,Light& light1, Light& light2, Z_Buffer& zBuffer);
-	void gouraudShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer);
-	void phongShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer);
+	void flatShading(MeshModel& mesh, Color ambientLight,Light& light1, Light& light2, Z_Buffer& zBuffer, Vector4& eyePosition);
+	void gouraudShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer, Vector4& eyePosition);
+	void phongShading(MeshModel& mesh, Color ambientLight, Light& light1, Light& light2, Z_Buffer& zBuffer, Vector4& eyePosition);
 
 	Vector4 getNewBarycentricCrd(Triangle& T,Vector4& bCrd, eScanConvMovement M, unsigned int numOfMoves);
 	double helpGNBC(Triangle& T, int a, int b, double x, double y);
 
-	Color getFlatColor(Triangle& T,Material& M, Color& ambientLight, Light& light1, Light& light2);
-	Color getVertxColor(vertexInfo& vInfo, Material& M, Color& ambientLight, Light& light1, Light& light2);
+	Color getFlatColor(Triangle& T,Material& M, Color& ambientLight, Light& light1, Light& light2, Vector4& eyePosition);
+	Color getVertxColor(vertexInfo& vInfo, Material& M, Color& ambientLight, Light& light1, Light& light2, Vector4& eyePosition);
 	
 
 	Color clacDiffuseLight(Vector4& point, Vector4 & normal, Light& light, double Kd);
-	Color clacSpecularLight(Vector4& point, Vector4& normal, Light& light, double Ks, double specularExp);
+	Color clacSpecularLight(Vector4& point, Vector4& normal, Light& light, double Ks, double specularExp, Vector4& eyePosition);
 };
 
 
